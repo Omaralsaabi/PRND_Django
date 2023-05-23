15466d2e53ab07e9071cdb46bf16044331a06d41
Omaralsaabi <prof.omaralsaabi@gmail.com>
2023-05-23 13:54:35 +0300

    App adevertisements added

---
+++ advertisements/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Ad(models.Model):
+    user = models.ForeignKey("account.User", verbose_name=("user"), on_delete=models.CASCADE)
+    car = models.ForeignKey("core.Car", verbose_name=("car"), on_delete=models.CASCADE)
+    price = models.IntegerField(max_length=50)
+    date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='images/')
+    description = models.TextField()
+    likes = models.ManyToManyField("account.User", related_name='liked_ads')
+    comments = models.ManyToManyField("account.User", through='Comment', related_name='commented_ads')
+
+
+
+class Comment(models.Model):
+    user = models.ForeignKey("account.User", on_delete=models.CASCADE)
+    ad = models.ForeignKey(Ad, on_delete=models.CASCADE)
+    text = models.TextField()
